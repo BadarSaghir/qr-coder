8a2340a8ee7b9d70b8c44444ac3509aa56ad213d
SardarBadar <dataentrybadar@gmail.com>
2022-11-05 16:00:52 +0500

    toggle selection

--- src/components/Navbar/store/navStore.tsx
+++ src/components/Navbar/store/navStore.tsx
@@ -3,12 +3,13 @@ import { IItem } from "../elements";
 
 export const useNavStore = create<{
   items: IItem[];
-
+  selected: boolean;
   addNavItem: (item: IItem) => void;
   getNavItems: () => IItem[];
   init: (item: IItem[]) => void;
   toggleSelection: (idx: number) => IItem;
 }>((set, get) => ({
+  selected: false,
   items: [
     {
       icon: "Abc",
@@ -43,8 +44,22 @@ export const useNavStore = create<{
   getNavItems: () => get().items,
   toggleSelection: (idx: number) => {
     set((state) => {
-      state.items[idx].selected = state.items[idx].selected ? false : true;
-      return { items: [...state.items] };
+      // console.log("info state before", state.selected);
+      // console.log("info state item before", state.items[idx].selected);
+
+      if (state.selected == false && state.items[idx].selected == false) {
+        state.selected = true;
+        state.items[idx].selected = true;
+      } else if (state.selected == true && state.items[idx].selected == true) {
+        state.selected = false;
+        state.items[idx].selected = false;
+      } else {
+        state.items[idx].selected = false;
+      }
+      // console.log("info state after", state.selected);
+      // console.log("info state item after", state.items[idx].selected);
+
+      return { items: [...state.items], selected: state.selected };
     });
     return get().items[idx];
   },
