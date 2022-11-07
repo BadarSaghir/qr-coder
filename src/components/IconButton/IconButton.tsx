ccd173e38fff03e22583d20e61e81a493e38e1c9
SardarBadar <dataentrybadar@gmail.com>
2022-11-07 13:49:26 +0500

    icon button

--- src/components/IconButton/IconButton.tsx
+++ src/components/IconButton/IconButton.tsx
@@ -11,7 +11,7 @@ const IconButton = (props: Props) => {
   return (
     <Button
       onClick={props.onclick}
-      className="rounded-full py-6 px-12 text-xs font-bold  text-tertiary"
+      className="rounded-3xl sm:rounded-full px-[8vw] py-[3vh] sm:w-auto sm:py-4 sm:px-10 text-[0.9rem] sm:text-auto font-bold  text-tertiary"
       style={props.style}
     >
       {props.text}
