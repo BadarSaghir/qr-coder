1348550dc11b904e191ec65ff414e48807d64f5d
SardarBadar <dataentrybadar@gmail.com>
2022-11-21 18:27:20 +0500

    qr created

--- src/components/Navbar/Layouts/QRReadLayout.tsx
+++ src/components/Navbar/Layouts/QRReadLayout.tsx
@@ -1,19 +1,37 @@
-import React, { useState } from "react";
+import IconButton from "../../IconButton";
+import React, { useRef, useState } from "react";
 import DividerLayout from "../../Layouts/DividerLayout";
-import Input from "@mui/material/InputBase";
-type Props = {};
+import qr from "qr-scanner";
 
+type Props = {};
 const QRReadLayout = (props: Props) => {
-  const [text, setText] = useState("");
+  const [text, setText] = useState("Your Qr Output Will be here....");
+  const ref = useRef<HTMLInputElement>(null);
+  const generateText = () => {
+    if (ref.current?.files)
+      qr.scanImage(ref.current?.files[0]).then((e) => {
+        setText(e);
+      });
+  };
   return (
     <DividerLayout>
       <div>
-        <p>{text}</p>
+        <p className="pb-5">{text}</p>
+        <input
+          ref={ref}
+          className="bg-primary text-secondary rounded-2xl"
+          type={"file"}
+          placeholder="Upload your qr code here"
+          onChange={generateText}
+        />
+        {/* <IconButton
+          style={{ backgroundColor: "blue" }}
+          text="Click here To read "
+          onclick={}
+        /> */}
         {/* < /> */}
       </div>
-      <div>
-        <Input type="file" placeholder="Upload Your file here?" />
-      </div>
+
       {/* < /> */}
     </DividerLayout>
   );
